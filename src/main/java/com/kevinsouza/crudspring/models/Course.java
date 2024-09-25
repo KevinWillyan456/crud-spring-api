44ed19ab506caf1f78d6d734b8b1001c5e00aac9
Kevin Souza <kevinwillyansouza@gmail.com>
2024-09-25 11:33:26 -0400

    chore: criação da configuração do banco de dados H2

--- src/main/java/com/kevinsouza/crudspring/models/Course.java
+++ src/main/java/com/kevinsouza/crudspring/models/Course.java
@@ -1,5 +1,7 @@
 package com.kevinsouza.crudspring.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,6 +19,7 @@ public class Course {
 	@Id
 	@Column(name = "id", unique = true)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty("_id")
 	private Long id;
 
 	@Column(name = "name", length = 200, nullable = false)
